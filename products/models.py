2533a2de66aa7f6f516ed0c16b9039f13cc95350
Kiarash <shahedikiarash@gmail.com>
2024-10-30 14:38:14 +0330

    add users and products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    seller = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="products")
+
+    def __str__(self):
+        return self.name
